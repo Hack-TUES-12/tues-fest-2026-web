3f03647c46494c55d4f6153c336459cb3237219d
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-03-01 00:14:00 +0200

    Style text

--- src/app/partners/page.tsx
+++ src/app/partners/page.tsx
@@ -11,8 +11,11 @@ export const revalidate = 0;
 
 export default function PartnersPage() {
 	return (
-		<section className="relative flex flex-col gap-14 overflow-x-visible px-8 pb-14 pt-14">
-			<h1 className="font-title text-center text-5xl">Нашите спонсори и партньори</h1>
+		<section className="relative flex flex-col items-center gap-14 overflow-x-visible px-8 pb-14 pt-14">
+            <div className='max-w-3xl gap-4 flex flex-col text-center'>
+			    <h1 className="font-title text-5xl">Нашите спонсори и партньори</h1>
+                <p className='text-lg'>Благодарим безкрайно на всички компании, които ни подкрепиха. Без вас събитията нямаше да бъдат възможни!</p>
+            </div>
 			<div className="mx-auto grid w-full max-w-screen-2xl grid-cols-1 place-items-center">
 				<OmegaSponsor />
 				<Sponsors />
