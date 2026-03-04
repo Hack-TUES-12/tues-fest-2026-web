651779b131d284ec58d84787f4c4143d55495d84
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-03-05 00:48:06 +0200

    Button gradients

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,12 +9,14 @@ const buttonVariants = cva(
 	{
 		variants: {
 			variant: {
-				default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
+				default: 'bg-gradient-to-br from-primary to-primary-end text-primary-foreground shadow-xs hover:opacity-90',
+				secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
+				muted: 'bg-muted text-muted-foreground shadow-xs hover:bg-muted/80',
+				accent: 'bg-accent text-accent-foreground shadow-xs hover:bg-accent/80',
 				destructive:
 					'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
 				outline:
 					'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
-				secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
 				ghost: 'hover:bg-foreground/30 hover:text-foreground',
 				link: 'text-primary underline-offset-4 hover:underline',
 			},
