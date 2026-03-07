731beb590a9e5c1c6be2a6f51f26384d8f2b4570
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-03-07 17:06:03 +0200

    Color based decorations

--- src/partials/projects/project-container.tsx
+++ src/partials/projects/project-container.tsx
@@ -1,15 +1,33 @@
-export function ProjectContainer({ children }: { children: React.ReactNode }) {
+import type { ProjectCategory } from '@/constants/projects';
+
+const CATEGORY_CIRCLE: Record<ProjectCategory | 'all', string> = {
+	all: '/decorations/blue-circle.svg',
+	embedded: '/decorations/purple-circle.svg',
+	software: '/decorations/green-circle.svg',
+	networks: '/decorations/orange-circle.svg',
+	battlebot: '/decorations/blue-circle.svg',
+};
+
+export function ProjectContainer({
+	children,
+	category = 'all',
+}: {
+	children: React.ReactNode;
+	category?: ProjectCategory | 'all';
+}) {
+	const circle = CATEGORY_CIRCLE[category];
+
 	return (
 		<div className="container mx-auto space-y-5 px-3 pb-5 pt-10">
 			<img
-				src="/decorations/purple-circle.svg"
+				src={circle}
 				alt=""
 				aria-hidden="true"
 				className="pointer-events-none fixed left-0 top-0 min-w-lg w-[60vw] -translate-x-1/2 -translate-y-1/2 select-none"
 				style={{ zIndex: -1 }}
 			/>
-				<img
-				src="/decorations/purple-circle.svg"
+			<img
+				src={circle}
 				alt=""
 				aria-hidden="true"
 				className="pointer-events-none fixed bottom-0 right-0 min-w-sm w-[45vw] translate-x-1/2 translate-y-1/2 select-none"
