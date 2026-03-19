7fc4ffce6dbf97f9bb4109a83c2308aedc282e27
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-03-19 11:27:07 +0200

    Better photos

--- src/info/folders.ts
+++ src/info/folders.ts
@@ -8,13 +8,13 @@ import tf2019 from './folders_images/tf2019_img1.jpg';
 import tf2019_2 from './folders_images/tf2019_img2.jpg';
 import tf2020 from './folders_images/tf2020_img1.png';
 import tf2020_2 from './folders_images/tf2020_img2.jpg';
-import tf2021 from './folders_images/tf2021_img1.jpg';
+import tf2021 from './folders_images/tf2021_new.webp';
 import tf2021_2 from './folders_images/tf2021_img2.png';
-import tf2022 from './folders_images/tf2022_img1.jpg';
+import tf2022 from './folders_images/tf2022_new.webp';
 import tf2022_2 from './folders_images/tf2022_img2.jpg';
 import tf2023_2 from './folders_images/tf2023_battlebots.jpg';
-import tf2023 from './folders_images/tf2023_ekip.jpg';
-import tf2024_3 from './folders_images/tf2024_3.jpg';
+import tf2023 from './folders_images/tf2023_new.webp'
+import tf2024_3 from './folders_images/tf2024_new.webp';
 import tf2024 from './folders_images/tf2024_img1.jpg';
 import tf2025 from './folders_images/tf2025.jpg'
 
