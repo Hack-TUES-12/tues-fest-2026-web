27db4f28c22f3664979a875b7ec852dd3537dcd2
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-04-12 21:12:27 +0300

    Add sumup description

--- src/constants/home/sponsors.ts
+++ src/constants/home/sponsors.ts
@@ -99,7 +99,7 @@ const ALPHA_SPONSORS = [
 		logo: sumupLogo,
 		url: 'https://www.sumup.com/bg-bg/',
 		description:
-			`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+			`SumUp е глобална финтех компания, основана през 2012 г., с мисия да подкрепя малкия бизнес. Помагаме на над 4 милиона търговци в 37 пазара с инструменти за старт, управление и растеж - от картови терминали до бизнес сметки, фактуриране и онлайн магазини, всичко в едно приложение.`,
 	},
 	{
 		name: 'Telelink Business Services',
