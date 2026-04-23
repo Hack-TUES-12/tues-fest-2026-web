915e348b8515400f3b5ee82ec3c83baae25691ad
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-04-23 10:54:12 +0300

    Update Kaldata

--- src/constants/home/sponsors.ts
+++ src/constants/home/sponsors.ts
@@ -58,6 +58,7 @@ const ttlMedia = '/media/tll.svg';
 const inzhenerBgLogo = '/media/inzhener-bg.webp';
 const devstylerArticleLogo = '/media/devstyler.svg';
 const managerLogo = '/media/manager.webp';
+const kaldataLogo = '/media/kaldata.svg'
 const darikBusinessReviewLogo = '/media/dbr.webp';
 const bloombergLogo = '/media/bloomberg.webp';
 const btvLogo = '/media/btv.png';
@@ -525,6 +526,14 @@ const MEDIA_PARTNERS = [
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
+	{
+	  name: "Kaldata",
+	  logo: kaldataLogo,
+	  url: "https://www.kaldata.com/tag/хакатон",
+	  description:
+		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+	  supportedEditions: [],
+	},
 	// {
 	// 	name: 'Businessnovinite.bg',
 	// 	logo: btvLogo,
@@ -548,14 +557,6 @@ const MEDIA_PARTNERS = [
 	// 	description:
 	// 		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	// 	supportedEditions: [],
-	// },
-	// {
-	//   name: "Kaldata",
-	//   logo: kaldataLogo,
-	//   url: "https://www.kaldata.com/tag/хакатон",
-	//   description:
-	// 	"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-	//   supportedEditions: [],
 	// },
 	// {
 	//   name: "Мениджър",
