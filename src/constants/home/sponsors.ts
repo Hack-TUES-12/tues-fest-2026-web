93ad29eeca0849025ea796e825a1d07d8d3ede81
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-04-23 11:00:17 +0300

    Add the recursive

--- src/constants/home/sponsors.ts
+++ src/constants/home/sponsors.ts
@@ -60,6 +60,7 @@ const devstylerArticleLogo = '/media/devstyler.svg';
 const managerLogo = '/media/manager.webp';
 const kaldataLogo = '/media/kaldata.svg'
 const economyBgLogo = '/media/economy-bg.webp';
+const theRecursiveLogo = '/media/the-recursive.svg'
 const darikBusinessReviewLogo = '/media/dbr.webp';
 const bloombergLogo = '/media/bloomberg.webp';
 const btvLogo = '/media/btv.png';
@@ -538,6 +539,14 @@ const MEDIA_PARTNERS = [
 		name: 'Economy.bg',
 		logo: economyBgLogo,
 		url: 'https://economy.bg/',
+		description:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		supportedEditions: [],
+	},
+	{
+		name: 'The Recursive',
+		logo: theRecursiveLogo,
+		url: 'https://therecursive.com/',
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 		supportedEditions: [],
